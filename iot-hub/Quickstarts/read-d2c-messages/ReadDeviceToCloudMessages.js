460bff937fcbc4b521e548f9e5a3ace9d434fcd6
Dominic Betts <dobett@microsoft.com>
2018-05-22 15:00:02 +0100

    Update to use latest event hubs sdk

--- iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
+++ iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 'use strict';
 
 // Connection string for the IoT Hub service
@@ -8,29 +5,32 @@
 // NOTE:
 // For simplicity, this sample sets the connection string in code.
 // In a production environment, the recommended approach is to use
-// an environment variable to make it available to your application
-// or use an x509 certificate.
+// an environment variable to make it available to your application.
 // https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
 //
-// Using the Azure CLI:
-// az iot hub show-connection-string --hub-name {YourIoTHubName} --output table
-var connectionString = '{Your service connection string here}';
+// az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+const eventHubsCompatibleEndpoint = '{Your Event Hubs-compatible endpoint}';
+
+// az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+const eventHubsCompatibleName  = '{Your Event Hubs-compatible name}';
+
+// az iot hub policy show --name iothubowner --query primaryKey --hub-name {your IoT Hub name}
+const primaryKey = '{your iothubowner primary key}'
+
+const eventHubsCompatibleConnectionString = `Endpoint=${eventHubsCompatibleEndpoint};SharedAccessKeyName=iothubowner;SharedAccessKey=${primaryKey}`;
+
 
 // Using the Node.js SDK for Azure Event hubs:
 //   https://github.com/Azure/azure-event-hubs-node
 // The sample connects to an IoT hub's Event Hubs-compatible endpoint
 // to read messages sent from a device.
-var EventHubClient = require('azure-event-hubs').Client;
-
-var printError = function (err) {
-  console.log(err.message);
-};
+const { EventHubClient, EventPosition } = require('azure-event-hubs');
 
 // Display the message content - telemetry and properties.
 // - Telemetry is sent in the message body
 // - The device can add arbitrary application properties to the message
 // - IoT Hub adds system properties, such as Device Id, to the message.
-var printMessage = function (message) {
+const onMessage = (message) => {
   console.log('Telemetry received: ');
   console.log(JSON.stringify(message.body));
   console.log('Application properties (set by device): ')
@@ -40,18 +40,26 @@ var printMessage = function (message) {
   console.log('');
 };
 
+const client = EventHubClient.createFromConnectionString(eventHubsCompatibleConnectionString, eventHubsCompatibleName);
+
 // Connect to the partitions on the IoT Hub's Event Hubs-compatible endpoint.
 // This example only reads messages sent after this application started.
-var client = EventHubClient.fromConnectionString(connectionString);
-client.open()
-    .then(client.getPartitionIds.bind(client))
-    .then(function (partitionIds) {
-        return partitionIds.map(function (partitionId) {
-            return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
-                console.log('Created partition receiver: ' + partitionId)
-                receiver.on('errorReceived', printError);
-                receiver.on('message', printMessage);
-            });
-        });
+async function main() {
+  const onError = (err) => {
+    console.log("An error occurred on the receiver ", err);
+  };
+
+  client.getPartitionIds().then(partitionIds => {
+    partitionIds.map(async (partitionId) => {
+      console.log('Created partition receiver: ' + partitionId);
+      const receiveHandler = client.receive(partitionId, onMessage, onError, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
+
+      // To stop receiving events later on...
+      await receiveHandler.stop();
     })
-    .catch(printError);
+  });
+}
+
+main().catch((err) => {
+  console.log(err);
+});
